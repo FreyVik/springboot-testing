70bd2214c001d449ce769744950410ba79fa9e63
FreyVik <yonkigamer96@gmail.com>
2022-11-17 20:38:26 +0100

    initial spring push

---
+++ src/test/java/com/freyvik/springboot_testing/rest/app/SpringbootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freyvik.springboot_testing.rest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
